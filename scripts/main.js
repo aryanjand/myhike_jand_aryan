a87f394ee4623468f7aa1d54080903a2ee00a143
Aryan Rocks <aryan.jand@gmail.com>
2022-03-08 14:30:31 -0800

    made the changes to the index.html, login.html, main.html, authentication.js adnn main.js

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+function read_display_Quote(){
+    //console.log("inside the function")
+    //get into the right collection
+    db.collection("quotes").doc("tuesday")
+    .onSnapshot(tuesdayDoc => {
+        console.log(tuesdayDoc.data());
+        document.getElementById("quote-goes-here").innerHTML=tuesdayDoc.data().quote;
+    })
+}
+read_display_Quote();
+
+function insertName() {
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if user is signed in:
+        if (user) {                                                                 
+            // Do something for the current logged-in user here: 
+            console.log(user.uid);
+            //go to the correct user document by referencing to the user uid
+            currentUser = db.collection("users").doc(user.uid);
+            //get the document for current user.
+            currentUser.get()
+                  .then(userDoc => {
+               var user_Name = userDoc.data().name;
+               console.log(user_Name);
+               //method #1:  insert with html only
+               //document.getElementById("name-goes-here").innerText = user_Name;    //using javascript
+               //method #2:  insert using jquery
+               $("#name-goes-here").text(user_Name);                         //using jquery
+            })
+        } else {
+            // No user is signed in.
+        }
+    });
+}
+insertName();
